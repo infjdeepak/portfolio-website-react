c3b7c545c6a4318fe0011999e44830f9b68c5302
Deepak <sumitmanni22@gmail.com>
2023-02-20 14:14:48 +0530

    added about animation

--- src/animation.js
+++ src/animation.js
@@ -4,20 +4,60 @@ export const pageAnimation = {
     x: "100%",
   },
   show: {
-    // opacity: 1,
     y: 0,
     x: 0,
     transition: {
-      ease: "easeOut",
       duration: 0.8,
+      when: "beforeChildren",
+      staggerChildren: 0.2,
     },
   },
   exit: {
     y: 300,
     x: "-100%",
     transition: {
-      ease: "easeOut",
       duration: 0.6,
+    },
+  },
+};
+
+export const titleAnim = {
+  hidden: {
+    y: 200,
+  },
+  show: {
+    y: 0,
+    transition: {
+      ease: "easeOut",
+      duration: 0.7,
+    },
+  },
+};
+
+export const fade = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.8,
+    },
+  },
+};
+
+export const imageAnim = {
+  hidden: {
+    opacity: 0,
+    scale: 1.5,
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.7,
     },
   },
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,32 +2,33 @@ import home1 from "../img/home1.png";
 // import styled from "styled-components";
 import { About, Description, Hide, Image } from "../styles";
 import { motion } from "framer-motion";
+import { titleAnim, fade, imageAnim } from "../animation";
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <motion.div>
+        <div className="title">
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>come true.</motion.h2>
+            <motion.h2 variants={titleAnim}>come true.</motion.h2>
           </Hide>
-        </motion.div>
-        <p>
+        </div>
+        <motion.p variants={fade}>
           Get in touch with us if you have any ideas involving photography or
           videography. Amazingly skilled individuals are available here to
           assist you in accomplishing your goal..
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="man with camera" />
+        <motion.img variants={imageAnim} src={home1} alt="man with camera" />
       </Image>
     </About>
   );
